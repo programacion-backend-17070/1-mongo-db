50aff92104a19370a215defcb409e3db5439e691
lalomx <lalo0892@gmail.com>
2022-03-12 14:02:46 -0600

    admin middleware

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express")
-const app = express();
+const adminMiddleware = require("./middlewares/admin")
 const productsRouter = require("./routes/products")
+
+const app = express();
 const PORT = process.env.PORT || 8080
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 
-app.use("/api/productos", productsRouter)
+app.use("/api/productos",  adminMiddleware, productsRouter)
 
 app.listen(PORT, () => console.log("🚀 Server has started"))
---
+++ middlewares/admin.js
@@ -0,0 +1,16 @@
+const isAdmin = false
+
+module.exports = (req, res, next) => {
+  if (isAdmin) {
+    return next()
+  }
+  
+  if (req.method === "POST" || req.method === "PUT" || req.method === "DELETE") {
+    return res.status(401).send({
+      error: -1,
+      message: "El usuario no tiene authorizacion"
+    })
+  }
+
+  next()
+}
